140362d699b90dfe60f497cd8404ef3b7cc5cd4c
fmc <fullmetalcache@gmail.com>
2017-03-27 18:14:09 -0600

    First working version of PowerStripper. Strips comments, replaces Write-Host with Write-Output, saves the stripped file, then creates a base64 version of the stripped file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace PowerStripper
 {
@@ -10,6 +8,83 @@ namespace PowerStripper
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please provide the to a file...");
+                return;
+            }
+
+            string strippedFile = "";
+            string encodedFile = "";
+
+            if (args[0].Contains("."))
+            {
+                strippedFile = args[0].Substring(0, args[0].IndexOf(".")) + "_stripped.ps1";
+                encodedFile = args[0].Substring(0, args[0].IndexOf(".")) + "_encoded.txt";
+            }
+            else
+            {
+                strippedFile = args[0] + "_stripped.ps1";
+                encodedFile = args[0] + "_encoded.txt";
+            }
+
+            stripComments(args[0], strippedFile);
+            b64Encode(strippedFile, encodedFile);
+        }
+
+        static void stripComments( string inFile, string outFile )
+        {
+            using (StreamWriter fso = new System.IO.StreamWriter(outFile))
+            {
+                bool startToken = false;
+
+                foreach (string line in File.ReadLines(inFile))
+                {
+                    if (!startToken)
+                    {
+                        if (line.Contains("<#"))
+                        {
+                            startToken = true;
+                            continue;
+                        }
+                        else
+                        {
+                            if (line.Length > 0)
+                            {
+                                string outLine = line;
+
+                                if (outLine.Contains("#"))
+                                {
+                                    int idx = outLine.IndexOf("#");
+                                    outLine = outLine.Substring(0, idx);
+                                }
+
+                                if (outLine.Length > 0)
+                                {
+                                    outLine = outLine.Replace("Write-Host", "Write-Output");
+
+                                    fso.WriteLine(outLine);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (line.Contains("#>"))
+                        {
+                            startToken = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void b64Encode( string inFile, string outFile )
+        {
+            using (StreamWriter fso = new System.IO.StreamWriter(outFile))
+            {
+                fso.Write(Convert.ToBase64String(Encoding.UTF8.GetBytes(File.ReadAllText(inFile))));
+            }
         }
     }
 }
